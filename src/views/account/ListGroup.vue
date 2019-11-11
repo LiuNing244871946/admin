cbad11af7679c4ba98c24a9c504ff665d6c945e8
lncode <ln244871946@163.com>
2019-11-11 14:32:47 +0800

    区分大小写

--- src/views/account/ListGroup.vue
+++ src/views/account/ListGroup.vue
@@ -9,6 +9,7 @@
       </el-form-item>
       <el-form-item class="submit">
         <el-button type="primary" @click="onSubmit">查询</el-button>
+        <el-button type="success" @click="addGroup"><i class="el-icon-plus" />添加管理员分组</el-button>
       </el-form-item>
     </el-form>
     <!-- 表格数据 -->
@@ -18,7 +19,7 @@
       <el-table-column label="最后修改日期" prop="modify_date"/>
       <el-table-column label="操作">
         <template slot-scope="scope">
-          <el-button type="text" style="color: #478FCA" @click="handleClick(scope.row)">操作</el-button>
+          <el-button type="text" style="color: #478FCA" @click="handleRow(scope.row)">操作</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -33,8 +34,18 @@
         @size-change="handleSizeChange"
         @current-change="handleCurrentChange" />
     </div>
-    <!-- 详情弹窗 -->
-
+    <!-- 添加分组弹窗 -->
+    <el-dialog :visible.sync="iv_dialog.show" :title="iv_dialog.title">
+      <el-form ref="addForm" :model="addForm" label-width="90px">
+        <el-form-item label="分组名：">
+          <el-input v-model="addForm.group_name" auto-complete="off" />
+        </el-form-item>
+        <el-form-item style="display:block;text-align:center;" label-width="0px">
+          <el-button @click="iv_dialog.show = false">取消</el-button>
+          <el-button type="primary" @click="submitForm('addForm')">确定</el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
   </div>
 </template>
 <script>
@@ -42,11 +53,11 @@ import request from '@/utils/request'
 export default {
   data() {
     return {
-      dialogVisible: false,
       formInline: {
         currPage: 1,
         pageSize: 10
       },
+      id:null,
       tableData: [],
       listLoading: true,
       pagination: {
@@ -54,6 +65,15 @@ export default {
         pageSizes: [10],
         pageSize: 0,
         tatal: 0
+      },
+      addForm:{
+        group_name:"",
+      },
+      iv_dialog:{
+        show: false,
+        title: null,
+        isEdit: false,
+        userId: null,
       }
     }
   },
@@ -72,7 +92,6 @@ export default {
         data: formInline
       }).then(response => {
         if (response.code === 200) {
-          console.log(response)
           const result = response.data
           that.pagination.pageSize = response.data.pageSize
           that.pagination.tatal = response.data.totalNum
@@ -82,6 +101,7 @@ export default {
           that.pagination.pageSize = 0
           that.pagination.tatal = 0
           that.tableData = []
+          this.$message.error(response.msg);
         }
       })
     },
@@ -100,9 +120,54 @@ export default {
       that.formInline.page = val
       that.getTableDatas()
     },
-    handleClick() {
-
-    }
+    handleRow(row) {
+      this.iv_dialog.show = true
+      this.iv_dialog.title = "修改分组信息"
+      this.iv_dialog.isEdit = true
+      this.id = row.id
+      this.addForm.group_name = row.group_name
+    },
+    addGroup(){
+      this.iv_dialog.show = true
+      this.iv_dialog.title = "添加管理员分组"
+      this.iv_dialog.isEdit = false
+    },
+    submitForm(formName){
+      let that = this
+      if(that.iv_dialog.isEdit){
+        this.postForm({ ...that.addForm , ...{id:this.id}},formName)
+      }else{
+        this.postForm(this.addForm,formName)
+      }
+      this.iv_dialog.isEdit = false
+    },
+    postForm(form,formName){
+      let that = this
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          request({
+            url: '/admin/index/putGroup',
+            method: 'post',
+            data: form
+          }).then(response => {
+            if (response.code === 200) {
+              this.$message({
+                message: '提交成功',
+                type: 'success'
+              })
+              that.iv_dialog.show = false
+              this.getTableDatas()
+              that.resetForm(formName)
+            } else {
+              this.$message.error(response.msg);
+            }
+          })
+        }
+      })
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
   }
 }
 </script>
